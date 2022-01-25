10694e64e7899496eb579386e2f0283d6e8c5372
Sumit Kaul <kaulsu@crowley.com>
2022-01-25 13:38:59 -0800

    update cdk pipeline code

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,49 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
-import { CodePipeline } from 'aws-cdk-lib/aws-events-targets';
-import { Code } from 'aws-cdk-lib/aws-lambda';
-import { CodeBuildStep, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { CodeBuildStep, CodePipeline , CodePipelineSource } from 'aws-cdk-lib/pipelines';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
+
 
 export class WorkshopPipelineStack extends cdk.Stack{
     constructor(scope: Construct, id: string , props?: cdk.StackProps ){
         super(scope, id, props);
 
-    new codecommit.Repository(this, 'WorkshopRepo', {
+    const repo = new codecommit.Repository(this, 'WorkshopRepo', {
         repositoryName: 'WorkshopRepo'
     });
 
-    // const pipeline = new CodePipeline( this, 'Pipeline', {
-    //     pipelineName: 'WorkshopPipeline',
-    //     synth: new CodeBuildStep('SynthStep' , { 
-    //             input: CodePipelineSource.codeCommit( repo , 'master'),
-    //             installCommands:[
-    //                 'npm install -g aws-cdk'
-    //             ],
-    //             commands:[
-    //                 'npm ci',
-    //                 'npm run build', 
-    //                 'npm cdk synth'
-    //             ]
-    //         }
-    //     )
-    // });
-
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline( this, 'Pipeline', {
         pipelineName: 'WorkshopPipeline',
-        synth: new CodeBuildStep('SynthStep', {
-                input: CodePipelineSource.codeCommit(repo, 'master'),
+        synth: new CodeBuildStep('SynthStep' , { 
+                input: CodePipelineSource.codeCommit( repo, 'master'),
                 installCommands: [
                     'npm install -g aws-cdk'
                 ],
                 commands: [
                     'npm ci',
-                    'npm run build',
-                    'npx cdk synth'
+                    'npm run build', 
+                    'npm cdk synth'
                 ]
             }
         )
     });
-
     }
 }
