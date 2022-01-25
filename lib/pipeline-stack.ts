7e0646a7023947da85dba5ef5b00f05033d44328
Sumit Kaul <kaulsu@crowley.com>
2022-01-25 10:53:52 -0800

    adding pipeline stage

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import { CodePipeline } from 'aws-cdk-lib/aws-events-targets';
 import { Code } from 'aws-cdk-lib/aws-lambda';
+import { CodeBuildStep, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class WorkshopPipelineStack extends cdk.Stack{
@@ -11,6 +13,37 @@ export class WorkshopPipelineStack extends cdk.Stack{
         repositoryName: 'WorkshopRepo'
     });
 
+    // const pipeline = new CodePipeline( this, 'Pipeline', {
+    //     pipelineName: 'WorkshopPipeline',
+    //     synth: new CodeBuildStep('SynthStep' , { 
+    //             input: CodePipelineSource.codeCommit( repo , 'master'),
+    //             installCommands:[
+    //                 'npm install -g aws-cdk'
+    //             ],
+    //             commands:[
+    //                 'npm ci',
+    //                 'npm run build', 
+    //                 'npm cdk synth'
+    //             ]
+    //         }
+    //     )
+    // });
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+        pipelineName: 'WorkshopPipeline',
+        synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.codeCommit(repo, 'master'),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            }
+        )
+    });
 
     }
 }
